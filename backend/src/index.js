047fef6958863393de6f78ffa721b88635281406
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-01-18 20:05:20 +0530

    signup, login and logout route written completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -8,8 +9,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello World !");
+  res.send("Hello world");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectDB();
