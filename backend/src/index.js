498748619eced75d651fef916e8b05877d21606f
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-01-23 18:53:06 +0530

    Check Auth endpoint completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // alows us to parse incoming requests from : req.body
+app.use(cookieParser()); // alows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
