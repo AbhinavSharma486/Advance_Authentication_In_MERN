d2b76cb55bbfe3e3e2f6e38eaf2af258d1a47d67
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-01-29 20:09:49 +0530

    Sign Up Page UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import FloatingShape from './components/FloatingShape.jsx';
+import SignUpPage from "./pages/SignupPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline text-green-600">
-          Welcome abhinav sharma
-        </h1>
-      </div >
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color='bg-green-500' size="w-64 h-64" top="-5%" left="10%" delay={0} />
+      <FloatingShape color='bg-emerald-500' size="w-48 h-48" top="70%" left="80%" delay={5} />
+      <FloatingShape color='bg-lime-500' size="w-32 h-32" top="40%" left="-10%" delay={2} />
+
+      <Routes>
+        <Route path='/' element={"Home"} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/verify-email' />
+        <Route path='/forgot-password' />
+        <Route path='/reset-password' />
+        <Route />
+      </Routes>
+    </div>
   );
 }
 
